2d181ab17a07390fec668ac9a05569434b83b50b
Siddharth Nand <snand233@gmail.com>
2024-03-08 20:22:59 -0800

    added paper and improvd mobile view

--- src/js/main.js
+++ src/js/main.js
@@ -1,37 +1,14 @@
 let body = document.body;
-let darkModeIcon = document.querySelector("#dark-mode-toggle i");
-let header = document.querySelector("#header");
 
-let darkMode = localStorage.getItem("latex-dark");
+let menuToggle = document.getElementById('menu-toggle');
+let navigation = document.getElementById('navigation');
 
 let main = () => {
-    if (darkMode === "true") changeUIDark();
+    menuToggle.addEventListener('click', toggleMenu);
 }
 
-let toggleDark = () => {
-    if (darkMode !== "true") {
-        localStorage.setItem("latex-dark", "true");
-    } else {
-        localStorage.setItem("latex-dark", "false");
-    }
-
-    changeUIDark();
-}
-
-let changeUIDark = () => {
-    body.classList.toggle("latex-dark");
-
-    darkModeIcon.classList.toggle("fa-sun");
-    darkModeIcon.classList.toggle("fa-moon");
-
-    if (darkModeIcon.classList.contains("fa-moon")) {
-        darkModeIcon.style.color = "black";
-        header.style.backgroundColor = "hsl(210, 20%, 98%)";
-    }
-    else {
-        darkModeIcon.style.color = "white";
-        header.style.backgroundColor = "hsl(0, 0%, 16%)";
-    }
+let toggleMenu = () => {
+    navigation.classList.toggle('show');
 }
 
 main();
